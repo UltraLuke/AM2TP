9418c4b5eb6db167878dcd4dbf512e0a527da24d
UltraLuke <lu.ez.ne@gmail.com>
2020-10-25 22:16:42 -0300

    Downgrade
    El proyecto de Unity era la 2019.3.9f1, la cambie a la version del colegio (2019.2.17f1)

---
+++ Assets/Scripts/Lucas/Grid.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HexarUtils;
+using System;
+
+public class Grid<TGridObject>
+{
+    public event EventHandler<OnGridValueChangedEventArgs> onGridValueChanged;
+    public class OnGridValueChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+
+    private int _width;
+    private int _height;
+    private float _cellSize;
+    private Vector3 _originPosition;
+    private TGridObject[,] _gridArray;
+    private TextMesh[,] _debugTextArray;
+
+    //Properties
+    public int Width => _width;
+    public int Height => _height;
+
+    public Grid(int w, int h, float cellSize, Vector3 originPos, Func<Grid<TGridObject>, int, int, TGridObject> createGridObject)
+    {
+        _width = w;
+        _height = h;
+        _cellSize = cellSize;
+        _originPosition = originPos;
+
+        _gridArray = new TGridObject[w, h];
+
+        for (int x = 0; x < _gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < _gridArray.GetLength(1); y++)
+            {
+                _gridArray[x, y] = createGridObject(this, x, y);
+            }
+        }
+        
+        bool showDebug = false;
+        if (showDebug)
+        {
+            _debugTextArray = new TextMesh[w, h];
+
+            for (int i = 0; i < _gridArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < _gridArray.GetLength(1); j++)
+                {
+                    _debugTextArray[i,j] = CustomUtilities.CreateWorldText(_gridArray[i, j]?.ToString(), null, GetWorldPosition(i, j) + new Vector3(cellSize, cellSize) * .5f, 20, Color.white, TextAnchor.MiddleCenter);
+                    Debug.DrawLine(GetWorldPosition(i, j), GetWorldPosition(i, j + 1), Color.white, 100f);
+                    Debug.DrawLine(GetWorldPosition(i, j), GetWorldPosition(i + 1, j), Color.white, 100f);
+                }
+            }
+            Debug.DrawLine(GetWorldPosition(0, h), GetWorldPosition(w, h), Color.white, 100f);
+            Debug.DrawLine(GetWorldPosition(w, 0), GetWorldPosition(w, h), Color.white, 100f);
+
+            onGridValueChanged += (object sender, OnGridValueChangedEventArgs eventArgs) => {
+                _debugTextArray[eventArgs.x, eventArgs.y].text = _gridArray[eventArgs.x, eventArgs.y]?.ToString();
+            };
+        }
+    }
+
+    
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * _cellSize + _originPosition;
+    }
+    public void GetXY(Vector3 worldPos, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPos - _originPosition).x / _cellSize);
+        y = Mathf.FloorToInt((worldPos - _originPosition).y / _cellSize);
+    }
+
+    public void TriggerGridObjectChanged(int x, int y)
+    {
+        if (onGridValueChanged != null) onGridValueChanged(this, new OnGridValueChangedEventArgs { x = x, y = y });
+
+    }
+
+    public void SetGridObject(int x, int y, TGridObject value)
+    {
+        if(x >= 0 && y >= 0 && x < _width && y < _height)
+        {
+            _gridArray[x, y] = value;
+
+            if (onGridValueChanged != null) onGridValueChanged(this, new OnGridValueChangedEventArgs { x = x, y = y });
+            //_debugTextArray[x, y].text = _gridArray[x, y].ToString();
+        }
+    }
+    public void SetGridObject(Vector3 worldPos, TGridObject value)
+    {
+        int x, y;
+        GetXY(worldPos, out x, out y);
+        SetGridObject(x, y, value);
+    }
+
+    public TGridObject GetGridObject(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < _width && y < _height)
+            return _gridArray[x, y];
+        else
+            return default(TGridObject);
+    }
+    public TGridObject GetGridObject(Vector3 worldPos)
+    {
+        int x, y;
+        GetXY(worldPos, out x, out y);
+        return GetGridObject(x, y);
+    }
+}
