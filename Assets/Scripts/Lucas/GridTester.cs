96608d42d72138a327d2490a14957630fe7392ad
UltraLuke <42553418+UltraLuke@users.noreply.github.com>
2020-10-26 21:17:46 -0300

    Ajustes grilla
    Se hacen ajustes de QA en la grilla. Ya está lista para ser utilizada para obtener sus posiciones.

--- Assets/Scripts/Lucas/GridTester.cs
+++ Assets/Scripts/Lucas/GridTester.cs
@@ -6,15 +6,24 @@ public class GridTester : MonoBehaviour
 {
     public int rows;
     public int columns;
-    public int floors;
+    //public int floors;
     public float cellSize;
     public Vector3 origPos;
 
 
     Grid<GameObject> _grid;
 
-    private void Start()
+    public Grid<GameObject> Grid { get => _grid; }
+
+    //private void Start()
+    //{
+    //    GenerateNewGrid();
+    //}
+
+    public void GenerateNewGrid()
     {
-        _grid = new Grid<GameObject>(columns, rows, floors, cellSize, origPos, (Grid<GameObject> g, int x, int y, int z) => null);
+        _grid = new Grid<GameObject>(columns, rows, 1, cellSize, origPos, (Grid<GameObject> g, int x, int y, int z) => null);
     }
+
+    
 }
