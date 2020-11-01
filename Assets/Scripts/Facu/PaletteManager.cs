6798746918fbe95c5da2a3ec75358f601f7c0ffe
FacundoLv <FacundoLv@users.noreply.github.com>
2020-11-01 15:43:12 -0300

    Facu | PaletteManager y PaletteObject agregados
    Agregados un manager in un SO para guardar y cargar info de las paletas.
    Los metodos son los siguientes:
    PaletteObject:
    - SetName
    - AddObject (agrega un objeto a la paleta si no está ya agregado)
    -RemoveObject (quita un objeto de la paleta si existe)
    (tambien se pueden crear paletas con click derecho Create>LevelEditor>NewPalette)
    PaletteManager:
    - CreatePalette (Crea un palette con el nombre dado en Resources/Palettes)
    - DeletPalette
    - LoadPalette (Carga un palette con el nombre dado de la carpeta de recursos, tambien tiene un overload para cargar palettes de otros paths)

---
+++ Assets/Scripts/Facu/PaletteManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class PaletteManager
+{
+    public static void CreatePalette(string paletteName)
+    {
+        string path = string.Format("Assets/Resources/Palettes/{0}.asset", paletteName);
+
+        AssetDatabase.CreateAsset(
+            ScriptableObject.CreateInstance<PaletteObject>()
+            .SetName(paletteName), path);
+    }
+
+    public static void DeletePalette(string paletteName)
+    {
+        string path = string.Format("Assets/Resources/Palettes/{0}.asset", paletteName);
+        AssetDatabase.DeleteAsset(path);
+    }
+
+    //TODO: establecer checks en caso de que no esté la carpeta
+    public static PaletteObject LoadPalette(string paletteName)
+    {
+        return Resources.Load<PaletteObject>("Palettes/" + paletteName);
+    }
+
+    public static PaletteObject LoadPalette(string paletteName, string path)
+    {
+        return (PaletteObject)AssetDatabase.LoadAssetAtPath(path + paletteName, typeof(PaletteObject));
+    }
+}
---
+++ Assets/Scripts/Facu/PaletteObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewPalette", menuName ="LevelEditor/NewPalette")]
+public class PaletteObject : ScriptableObject
+{
+    public List<GameObject> content = new List<GameObject>();
+    public string paletteName;
+
+    public PaletteObject SetName(string name)
+    {
+        this.paletteName = name;
+        return this;
+    }
+
+    public void AddObject(GameObject gameObject)
+    {
+        if (content.Contains(gameObject))
+            return;
+        content.Add(gameObject);
+    }
+
+    public void RemoveObject(GameObject gameObject)
+    {
+        if (content.Contains(gameObject))
+            return;
+        content.Remove(gameObject);
+    }
+}
