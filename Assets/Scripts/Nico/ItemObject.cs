bc7547572a82dd8566cedf4ada18e569110066c4
nikorno <nikornogame@gmail.com>
2020-11-01 20:44:58 -0300

    Nico Update
    Creados dos SO para guardar la informacion, uno de los objetos el otro de la grilla
    (Para el proximo commit voy a hacer uno solo que guarda todo)
    Se crea en la barra superior una pestania CustomTools con un Load y un Save que correran los scripts para guardar la informacion de una scena y poder cargarlo en otra.
    Quedan pendientes verificaciones de carpetas.

---
+++ Assets/Scripts/Nico/ItemObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : ScriptableObject
+{
+    public string myName;
+    public string path; //Ruta del Prefab
+    public Vector3 position;
+    public Vector3 scale;
+    public Quaternion rotation;
+
+    public void Setter(string _name, string PrefabPath, Vector3 _position, Vector3 _scale, Quaternion _rotation)
+    {
+        myName = _name;
+        path = PrefabPath;
+        position = _position;
+        scale = _scale;
+        rotation = _rotation;
+    }
+}
